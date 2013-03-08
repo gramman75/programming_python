475aaf43b93546f240da218b40ae51f810f95582
gramman75 <gramman75@gmail.com>
2013-03-08 20:22:23 +0900

    111

--- MoveApplication/moviedata.py
+++ MoveApplication/moviedata.py
@@ -9,16 +9,17 @@ Created on 2013. 3. 7.
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
+import bisect
 
 
 class Movie():
     '''
     Movie 객체
     '''
-    def __init__(self, title=None, year=None, mins=0, acquired=None, notes=None):
+    def __init__(self, title=None, year=None, minutes=0, acquired=None, notes=None):
         self.title = title
         self.year = year
-        self.mins = mins
+        self.minutes = minutes
         self.acquired = acquired if acquired is not None else QDate.currentDate()
         self.notes = notes
 
@@ -26,31 +27,234 @@ class MovieContainer():
     '''
     Movie 객체에 대한 속성과 Method(save, load, count ...)
     '''
+    
+    MAGIC_NUMBER = 0x3051E
+    FILE_VERSION = 100
     def __init__(self):
         '''
         초기 사용 변수
         __movieFromID = {} : id : Movie 객체를 저장
         '''
         self.__fname = QString()    
-        self.__movies =[]
-        self.__movieFromId = {}
-        self.__dirty = False
+        self.__movies =[]              #[key, movie]가 list로 보관됨.
+        self.__movieFromId = {}        # id : Movive가 Dictionary로 보관됨.
+        self.__dirty = False           # data변경시 True
         
     def __len__(self):
         return len(self.__movies)
+    
+    def __iter__(self):
+        for pair in iter(self.__movies):
+            yield pair[1]
+    
+    def filename(self):
+        return self.__fname
+    
+    def setFilename(self, fname):
+        self.__fname = fname
     
     def clear(self, clearFilename=True):
         '''
         모든 Data를 Clear
         '''
-        self.__movies = []
-        self.__movieFromId = {}
+        self.__movies = [] 
+        self.__movieFromId = {} 
         if clearFilename:
             self.__fname = QString()
         self.__dirty = False
         
         
+    def add(self, movie):
+        '''
+        1. Movie object를 __movies에 추가
+        '''
+        if id(movie) in self.__movieFromId:
+            return False
+        
+        key = self.key(movie.title, movie.year)
+        bisect.insort_left(self.__movies, [key, movie])
+        self.__movieFromId[id(movie)] = movie
+        self.__dirty = True
+        return True
+        
+    def delete(self, movie):
+        '''
+        2. movie object를 __movies에서 삭제
+        '''
+        if id(movie) not in self.__movieFromId:
+            return False
+        key = self.key(movie.title, movie.year)
+        i = bisect.bisect_left(self.__movies, key)
+        del self.__movies[i]
+        del self.__movieFromId[id(movie)]
+        self.__dirty = True
+        return True
     
+    def updateMovie(self, movie, title, year, minutes = None, notes=None):
+        '''
+        3. movie object를 수정함.
+           title과 year가 수정되었을 경우에는 재정렬
+        '''
+        if minutes is not None:
+            movie.minutes = minutes
+            
+        if notes is not None:
+            movie.notes = notes
+            
+        if movie.title != title or movie.year != year:
+            key = self.key(title, year)
+            i =  bisect.bisect_left(self.__movies, [key,movie])
+            self.__movies[i][0] = self.key(title, year)
+            movie.title = title
+            movie.year = year
+            self.__movies.sort()
+        
+        self.__dirty = True
+    
+    def save(self, fname=QString()):
+        '''
+        4. 확장자에 따라 저장방법 분기
+        '''
+        if not fname.isEmpty():
+            self.__fname = fname
+        
+        if self.__fname.endswith('.mqb'):
+            self.saveQDataStream()
+            
+    def load(self,fname=QString()):
+        '''
+        5. 확장자에 따라 Load방법 분기
+        '''
+        if not fname.isEmpty():
+            self.__fname = fname
+        
+        if self.__fname.endsWith('.mqb'):
+            return self.loadQDataStream()   
+        
+        return False, "Failed to load: invalid file extension"       
+               
+    def saveQDataStream(self):
+        '''
+        6. QDataStream을 이용하여 저장
+        '''
+        
+        fh = None
+        error = None
+        
+        try:
+            '''
+            6.1 File Open
+            '''
+            fh = QFile(self.__fname)
+            if not fh.open(QIODevice.WriteOnly):
+                raise IOError, unicode(fh.errorString())
+            '''
+            6.2 Stream생성
+            '''
+            stream = QDataStream(fh)
+            '''
+            6.3 Binary파일 쓰기
+            '''
+            stream.writeInt32(MovieContainer.MAGIC_NUMBER)
+            stream.writeInt32(MovieContainer.FILE_VERSION)
+            '''
+            6.3 Strem 버전
+            '''
+            stream.setVersion(QDataStream.Qt_4_2)
+            
+            '''
+            6.4 Binary File읽기, String << or >> 사용
+            ''' 
+            
+            for key, movie in self.__movies:
+                stream << movie.title
+                stream.writeInt16(movie.minutes)
+                stream.writeInt16(movie.year)
+                stream << movie.acquired << movie.notes
+        except (IOError, OSError), e:
+            error = "Failed to save : %s" %e
+        finally:
+            if fh is not None:
+                fh.close()
+            if error is not None:
+                return False, error
+            self.__dirty = False
+        return True, "Saved %d movie record to %s" %(len(self.__movies), self.__fname)
+        
+    def loadQDataStream(self):
+        
+        error = None
+        fh = None
+        
+        try:
+            
+            fh = QFile(self.__fname)
+            if not fh.open(QIODevice.ReadOnly):
+                raise IOError, unicode(fh.errorString())
+            stream = QDataStream(fh)
+            magic = stream.readInt32()
+            
+            if magic != MovieContainer.MAGIC_NUMBER:
+                raise IOError, "Unrecongnized file type"
+            
+            version = stream.readInt32()
+            
+            if version < MovieContainer.FILE_VERSION:
+                raise IOError, "old and unreadable file format"
+            elif version > MovieContainer.FILE_VERSION:
+                raise IOError, "new and unreadable file format"
+            
+            stream.setVersion(QDataStream.Qt_4_3)
+            
+            
+            while not stream.atEnd():
+                
+                title = QString()
+                print title
+                acquired = QDate()
+                notes = QString()
+                
+                stream >> title
+                year = stream.readInt16()
+                minutes = stream.readInt16()
+                stream >> acquired >> notes
+                self.add(Movie(title, year, minutes, acquired, notes))
+
+        except (IOError, OSError), e:
+            error = "Failed to load : %s" %e
+        finally :
+            if fh is not None:
+                fh.close()
+            if error is not None:
+                return False, error
+            self.__dirty = False
+        
+        
+        print "movies count : %d" % (len(self.__movies))
+        print "movies count : %s" % QFileInfo(self.__fname).fileName()
+        
+        return True, "Load %d movie record to %s" %(len(self.__movies), QFileInfo(self.__fname).fileName() ) 
+              
+    
+    def key(self, title, year):
+        text = unicode(title).lower()
+        
+        if text.startswith("a "):
+            text = text[2:]
+        elif text.startswith("the "):
+            text = text[4:]
+        elif text.startswith("an "):
+            text = text[3:]
+        
+        parts = text.split(" ",1)
+        if parts[0].isdigit():
+            text = "%08d" %int(parts[0])
+            text += text + parts[1]
+        return "%s\t%d" %(text.replace(" ", ""), year)
+            
+        
+        
+        return None 
 
 def intFromQStr():
     pass
--- MoveApplication/mymovies.py
+++ MoveApplication/mymovies.py
@@ -15,7 +15,7 @@ Created on 2013. 3. 7.
 import sys
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
-import moviedata 
+import moviedata , qrc_resources
 
 class MainWindow(QMainWindow):
     
@@ -25,8 +25,86 @@ class MainWindow(QMainWindow):
         self.movies = moviedata.MovieContainer() #self로 선언을 하면 다른 함수에서 사용이 가능
         self.table = QTableWidget()
         self.setCentralWidget(self.table)
+        
+        # 상태바에 표시될 Label
+        
+        self.status = self.statusBar()
+        self.status.setSizeGripEnabled(True)        
+        self.status.showMessage("Ready", 5000)
+        
+        '''
+        5. action을 생성하고 menu와 toolbar에 action 추가
+        '''
+        fileNewAction = self.createAction('&New...', self.fileNew, QKeySequence.New,'filenew', u'새로운 파일을 생성합니다.')
+        fileOpenAction = self.createAction('&Open...', self.fileOpen, QKeySequence.Open,'fileopen', u'기존 파일을 불러옵니다.')
+        fileSaveAction = self.createAction('&Save', self.fileSave, QKeySequence.Save,'filesave', u'변경사항을 저장합니다.')
+        fileSaveAsAction = self.createAction('Save As...', self.fileSaveAs, QKeySequence.SaveAs,'filesaveas', u'다른 이름으로 저장합니다.')
+        fileQuitAction = self.createAction('&Quit', self.close,'Ctrl+Q', 'filequit', u'프로그램을 종료합니다.')
+        
+        editAddAction = self.createAction('Add...', self.editAdd, 'Ctrl+A','editadd', u'새로운 영화정보를 추가합니다.')
+        editDeleteAction = self.createAction('Delete', self.editDelete, 'Ctrl+D','editdelete', u'선택한 영화정보를 삭제합니다.')
+        editEditAction = self.createAction('&Edit...', self.editEdit, 'Ctrl+E','editedit', u'선택한 영화정보를 수정합니다.')
+        
+        
+        helpAboutAction = self.createAction('&About Image Changer', self.helpAbout, None, None, 'About this application')
+        '''
+        6. menu를 생성하고 Action을 추가
+        '''
+        
+        fileMenu = self.menuBar().addMenu('&File')
+        editMenu = self.menuBar().addMenu('&Edit')
+        helpMenu = self.menuBar().addMenu('&Help')
+        
+        self.addActions(fileMenu,(fileNewAction, fileOpenAction, fileSaveAction, fileSaveAsAction,None,fileQuitAction))
+        self.addActions(editMenu,(editAddAction, editDeleteAction, editEditAction))
+        self.addActions(helpMenu,(helpAboutAction,))
+        '''
+        7. toolbar를 생성하고 action추가 
+        '''
+        fileToolbar = self.addToolBar('File')
+        editToolbar = self.addToolBar('Edit')
+        self.addActions(fileToolbar,(fileNewAction, fileOpenAction, fileSaveAction, fileSaveAsAction))
+        self.addActions(fileToolbar,(editAddAction, editDeleteAction, editEditAction))
+        
         self.updateTable()
         
+    def fileNew(self):
+        if not self.okToContinue():
+            return False
+        
+    
+    def fileOpen(self):
+        
+        fname = QFileDialog.getOpenFileName(self, "load movie file","*.mqb","mqb")
+        if not fname.isEmpty():
+            ok, msg = self.movies.load(fname)
+            self.status.showMessage(msg,5000)
+            self.updateTable()
+    
+    def fileSave(self):
+        if self.movies.__fname is None:
+            return self.fileSaveAs()
+        else:
+            ok, msg = self.movies.save()
+            self.status.showMessage(msg, 5000)
+            return True
+    
+    def fileSaveAs(self):
+        pass
+    
+    def editAdd(self):
+        pass
+    
+    def editDelete(self):
+        pass
+    
+    def editEdit(self):
+        pass
+    
+    def helpAbout(self):
+        pass
+    
+    
     
     def updateTable(self, current=None): #Data변경이 발생할 때 마다 호출하여 Table값 변경
         self.table.clear()
@@ -34,6 +112,77 @@ class MainWindow(QMainWindow):
         self.table.setColumnCount(5)
         self.table.setHorizontalHeaderLabels(["Title","Year","Mins","Acquired","Notes"])
         
+        for row, movie in enumerate(self.movies):
+            item = QTableWidgetItem(movie.title)
+            self.table.setItem(row,0,item)
+            
+            item = QTableWidgetItem("{0}".format(movie.year))
+            item.setTextAlignment(Qt.AlignRight|Qt.AlignVCenter)
+            self.table.setItem(row,1,item)
+            
+            item = QTableWidgetItem("{0}".format(movie.minutes))
+            item.setTextAlignment(Qt.AlignRight|Qt.AlignVCenter)
+            self.table.setItem(row,2,item)
+            
+            item = QTableWidgetItem(movie.acquired.toString("ddd MMM d, yyyy"))
+            self.table.setItem(row,3,item)
+            
+            item = QTableWidgetItem(movie.notes)
+            self.table.setItem(row,4,item)
+            self.table.resizeColumnsToContents()
+        
+        
+    
+    def createAction(self, text, slot=None, shortcut=None, icon=None, tip=None, checkable=False, signal="triggered()"):
+        '''
+        3. 메뉴나 toolbar에 사용될 Action을 만든다.
+           Action 생성을 위한 util method 이다.
+           createAction
+        '''
+        action = QAction(text, self)
+        
+        if slot is not None:
+            self.connect(action, SIGNAL(signal), slot)
+        
+        if shortcut is not None:
+            action.setShortcut(shortcut)
+        
+        if icon is not None:
+            action.setIcon(QIcon(':/%s.png' %icon))
+        
+        if tip is not None:
+            action.setToolTip(tip)
+            action.setStatusTip(tip)
+        
+        if checkable:
+            action.setCheckable(True)
+        
+        return action
+        
+    def addActions(self, target, actions):
+        '''
+        4. menu나 toolbar에 여러개의 Action을 add하기 위한 Util Method
+           target : menu나 toolbar
+           actions: list type of action objects
+        '''        
+        for action in actions:
+            if action is None:
+                target.addSeparator()
+            else:
+                target.addAction(action)
+                
+    def okToContinue(self):
+        if self.movies.__dirty:
+            reply = QMessageBox.question(self, "Question", "변경된 내용이 있습니다. 저장하시겠습니까?", QMessageBox.Ok|QMessageBox.No|QMessageBox.Cancel)
+            if reply == QMessageBox.Cancel:
+                return False
+            elif reply == QMessageBox.Ok:
+                return self.fileSave()
+        
+        return True
+                
+                 
+    
 
 
 
